6a262ab5d6d5f87055ab828033ff4cf45bcac987
Helge <helge.dzierzon@brockmann-consult.de>
2018-08-24 14:14:32 +0200

    Setup basic search interface.

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,40 @@ import * as React from 'react';
 import './App.css';
 
 import logo from './logo.svg';
+import { SearchField } from "./SearchField";
+import { DataTable } from "./DataTable";
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+interface AppProps {
+    queryString: string;
+}
+
+class App extends React.PureComponent<AppProps> {
+
+    handleQueryStringChange = (queryString: string) => {
+        alert(queryString);
+    };
+
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <div className="App-main">
+                    <SearchField queryString={this.props.queryString}
+                                 onQueryStringChange={this.handleQueryStringChange}/>
+                    <br/>
+                    <br/>
+                    <DataTable numRows={100}/>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/SearchField.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { Button, InputGroup } from "@blueprintjs/core";
+import { FormEvent } from "react";
+
+/* TODO
+Handle enter event and focus event
+ */
+
+
+interface SearchFieldProps {
+    queryString: string;
+    onQueryStringChange: (queryString: string) => void;
+}
+
+
+interface SearchFieldState {
+    queryString: string;
+}
+
+
+export class SearchField extends React.PureComponent<SearchFieldProps, SearchFieldState> {
+
+
+    constructor(props: SearchFieldProps) {
+        super(props);
+        this.state = {queryString: props.queryString};
+    }
+
+    handleClick = () => this.props.onQueryStringChange(this.state.queryString);
+    handleChange = (event: FormEvent<HTMLInputElement>) => {
+        this.setState({
+            queryString: (event.target as HTMLInputElement).value
+        }, () => console.log(this.state));
+    };
+
+    render() {
+        return (
+            <InputGroup
+                disabled={false}
+                large={true}
+                leftIcon="search"
+                onChange={this.handleChange}
+                placeholder="query expression"
+                rightElement={<Button className="bp3-button bp3-minimal bp3-intent-primary bp3-icon-arrow-right"
+                                      onClick={this.handleClick}/>}
+                value={this.state.queryString}
+            />
+        );
+    }
+}
