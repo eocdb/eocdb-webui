f0fa54a77c63f146a7c889c4bea5dca30bc50557
Norman Fomferra <norman.fomferra@gmail.com>
2018-11-18 21:00:21 +0100

    formatted imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import {createLogger} from 'redux-logger';
-import { appReducer } from './reducers/appReducer';
+import { createLogger } from 'redux-logger';
 
+import { appReducer } from './reducers/appReducer';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 import 'typeface-roboto';
 
 const logger = createLogger({collapsed: true});
