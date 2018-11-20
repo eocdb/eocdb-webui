4df094645d2b8ce06d9da656a0beb98c6356c33a
Norman Fomferra <norman.fomferra@gmail.com>
2018-11-20 16:36:33 +0100

    Merge

--- src/components/DashPanels.tsx
+++ src/components/DashPanels.tsx
@@ -4,13 +4,7 @@ import HomePanel from './home/HomePanel';
 import SubmitPanel from './submit/SubmitPanel';
 import BrowsePanel from './browse/BrowsePanel';
 import AdminPanel from './admin/AdminPanel';
-import PanelSearch from '../containers/search/SearchPanel'; // TODO: dependency issue here!
-import HomePanel from "./home/HomePanel";
-import * as React from "react";
-import SubmitPanel from "./submit/SubmitPanel";
-import BrowsePanel from "./browse/BrowsePanel";
-import AdminPanel from "./admin/AdminPanel";
-import SearchPanel from "../containers/search/SearchPanel";
+import SearchPanel from '../containers/search/SearchPanel'; // TODO: dependency issue here!
 
 
 interface DashPanelsProps {
@@ -26,12 +20,7 @@ class DashPanels extends React.PureComponent<DashPanelsProps> {
     render() {
         return (
             <div>
-                <SearchPanel show={this.props.currentDrawer=='Search'} classes={this.props.classes}/>
-                <HomePanel show={this.props.currentDrawer=='Home'} classes={this.props.classes}/>
-                <SubmitPanel show={this.props.currentDrawer=='Submit'} classes={this.props.classes}/>
-                <BrowsePanel show={this.props.currentDrawer=='Browse'} classes={this.props.classes}/>
-                <AdminPanel show={this.props.currentDrawer=='Admin'} classes={this.props.classes}/>
-                <PanelSearch show={this.props.currentDrawer == 'Search'} classes={this.props.classes}/>
+                <SearchPanel show={this.props.currentDrawer == 'Search'} classes={this.props.classes}/>
                 <HomePanel show={this.props.currentDrawer == 'Home'} classes={this.props.classes}/>
                 <SubmitPanel show={this.props.currentDrawer == 'Submit'} classes={this.props.classes}/>
                 <BrowsePanel show={this.props.currentDrawer == 'Browse'} classes={this.props.classes}/>
