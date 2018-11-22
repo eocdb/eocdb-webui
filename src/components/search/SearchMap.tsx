bed4e6476a4f194a5fad59e13846918e7fb79cc7
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2018-11-22 11:06:51 +0100

    new base map(s)

--- src/components/search/SearchMap.tsx
+++ src/components/search/SearchMap.tsx
@@ -37,6 +37,14 @@ class SearchMap extends React.PureComponent<SearchMapProps> {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
+                <TileLayer
+                    url="https://gis.ngdc.noaa.gov/arcgis/rest/services/web_mercator/gebco_2014_contours/MapServer/tile/{z}/{y}/{x}"
+                    attribution="&copy; <a href=&quot;https://www.gebco.net/data_and_products/gridded_bathymetry_data/&quot;>GEBCO</a>, <a href=&quot;https://maps.ngdc.noaa.gov/&quot;>NOAHH</a> and contributors"
+                />
+                <TileLayer
+                    url="https://a.tiles.mapbox.com/v3/mapbox.natural-earth-2/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;https://www.naturalearthdata.com/&quot;>MapBox</a>, <a href=&quot;https://www.mapbox.com/&quot;>MapBox</a> and contributors"
+                />
 
                 <Marker position={this.props.position}>
                     <Popup>A pretty CSS3 popup.<br/>Easily customizable.</Popup>
