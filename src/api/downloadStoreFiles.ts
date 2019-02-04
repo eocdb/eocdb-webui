9cb49e900b75557320cd9d8a66c437c567a0adaf
Helge <helge.dzierzon@brockmann-consult.de>
2019-02-04 17:05:39 +0100

    - Added a collectidcomponent function as POST request parameter arfe passed as JSON   and not as query parameter

--- src/api/downloadStoreFiles.ts
+++ src/api/downloadStoreFiles.ts
@@ -10,16 +10,27 @@ export function downloadStoreFiles(apiServerUrl: string, datasetQuery: DatasetQu
 }
 
 
+export function collectIdComponents(datasetQuery: DatasetQuery) {
+    let queryComponents = undefined;
+
+    if (datasetQuery.datasetIds) {
+        queryComponents = { id_list: datasetQuery.datasetIds };
+    }
+
+    return queryComponents;
+}
+
 export function downloadStoreFilesByIds(apiServerUrl: string, datasetQuery: DatasetQuery): Promise<Blob> {
 
-    const queryComponents = collectComponents(datasetQuery);
+    const queryComponents = collectIdComponents(datasetQuery);
+    //const queryComponents = {id_list: ["5bf5451761d82d05dcd6cfb5", "5bf5451761d82d05dcd6cfb6"], docs: false};
 
     return callBlobApi(apiServerUrl + '/store/download',
         undefined,
         {
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
             method: 'POST',
             body: JSON.stringify(queryComponents),
