e7b62cbb1a4e3459c40ba0472f1e154b8bede66a
Helge <helge.dzierzon@brockmann-consult.de>
2019-02-04 14:14:21 +0100

    - SelectItems are now organised in this file to improve cobnfigurability of standard selects.

---
+++ src/components/search/SelectItems.ts
@@ -0,0 +1,10 @@
+export interface SelectItem{
+    key: string,
+    label: string,
+}
+
+export const wavelengthItems = [
+    {key: 'multispectral', label: 'Multispectral'},
+    {key: 'hyperspectral', label: 'Hyperspectral'},
+];
+
