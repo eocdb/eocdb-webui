1eb5160729a1dac2fde21bd30c23078ccb65fe9c
Helge <helge.dzierzon@brockmann-consult.de>
2019-02-04 14:11:22 +0100

    - Replaced left/right/bottom/top inputs with a new BBOx input

--- src/components/search/AdvancedSearchDialog.tsx
+++ src/components/search/AdvancedSearchDialog.tsx
@@ -6,12 +6,14 @@ import Slide, { SlideProps } from '@material-ui/core/Slide';
 
 import createStyles from "@material-ui/core/styles/createStyles";
 import { WithStyles } from "@material-ui/core";
-import TextField from "@material-ui/core/TextField/TextField";
 import Grid from "@material-ui/core/Grid/Grid";
 import DialogTitle from "@material-ui/core/DialogTitle/DialogTitle";
 import DialogActions from "@material-ui/core/DialogActions/DialogActions";
-import WaveLengthSelect from "../../containers/search/WaveLengthSelect";
-import { AdvancedSearchItem } from "../../types/advancedSearchDialog";
+import WaveLengthSelect from "./SimpleSelect";
+import BBoxInput from "./BBoxInput";
+import { LatLngBounds } from "leaflet";
+import { WavelengthsMode } from "../../api/findDatasets";
+import { wavelengthItems } from "./SelectItems";
 
 
 // noinspection JSUnusedLocalSymbols
@@ -41,17 +43,11 @@ export interface AdvancedSearchDialogProps extends WithStyles<typeof styles> {
     open: boolean;
     onClose: () => void;
 
-    logChange: (filterLog: AdvancedSearchItem[]) => void;
-    filterLog: AdvancedSearchItem[];
+    onBBoxChange: (selectedBounds: LatLngBounds) => void;
+    selectedBounds: LatLngBounds;
 
-    leftChange: (left: number) => void;
-    bottomChange: (bottom: number) => void;
-    rightChange: (right: number) => void;
-    topChange: (top: number) => void;
-    left: number;
-    bottom: number;
-    right: number;
-    top: number;
+    onWavelengthSelect: (item: string) => void;
+    selectedWavelength: WavelengthsMode;
 }
 
 
@@ -60,54 +56,9 @@ class AdvancedSearchDialog extends React.Component<AdvancedSearchDialogProps> {
         super(props);
     }
 
-    handleLogChange = (key: string, value: string) => {
-        let success = false;
-        const filterLog = this.props.filterLog.map(
-            (log: AdvancedSearchItem) => {
-                if (log.key == key) {
-                    log.value = value;
-                    success = true;
-                }
-                return log;
-            }
-        );
-
-        if (!success) {
-            filterLog.push({
-                key: key,
-                value: value,
-            });
-        }
-
-        this.props.logChange(filterLog);
-    };
-
-    handleLeftChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const left = event.target.valueAsNumber;
-        this.handleLogChange('left', left.toString());
-        this.props.leftChange(left);
-    };
-
-    handleBottomChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const bottom = event.target.valueAsNumber;
-        this.handleLogChange('bottom', bottom.toString());
-        this.props.bottomChange(bottom);
-    };
-
-    handleRightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const right = event.target.valueAsNumber;
-        this.handleLogChange('right', right.toString());
-        this.props.rightChange(right);
-    };
-
-    handleTopChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const top = event.target.valueAsNumber;
-        this.handleLogChange('top', top.toString());
-        this.props.topChange(top);
-    };
-
-    handleWaveLengthChange = () => {
-
+    handleWaveLengthSelect = (item: string) => {
+        console.log(item);
+        this.props.onWavelengthSelect(item);
     };
 
     render() {
@@ -121,47 +72,20 @@ class AdvancedSearchDialog extends React.Component<AdvancedSearchDialogProps> {
                     TransitionComponent={Transition}
                 >
                     <DialogTitle id="form-dialog-title">Advanced Search</DialogTitle>
-                    <Grid spacing={24} container direction={'row'} justify={'flex-start'} alignItems={"flex-start"}>
+                    <Grid spacing={16} container direction={'row'} justify={'flex-start'} alignItems={"flex-start"}>
                         <Grid item xs={12}>
-                            <TextField
-                                id={'bbox_left'}
-                                label={'Left'}
-                                variant={'outlined'}
-                                type={"number"}
-                                className={classes.searchField}
-                                onChange={this.handleLeftChange}
-                                value={this.props.left}
-                            />
-                            <TextField
-                                id={'bbox_bottom'}
-                                label={'Bottom'}
-                                variant={'outlined'}
-                                type={"number"}
-                                className={classes.searchField}
-                                onChange={this.handleBottomChange}
-                                value={this.props.bottom}
-                            />
-                            <TextField
-                                id={'bbox_right'}
-                                label={'Right'}
-                                variant={'outlined'}
-                                type={"number"}
-                                className={classes.searchField}
-                                onChange={this.handleRightChange}
-                                value={this.props.right}
-                            />
-                            <TextField
-                                id={'bbox_top'}
-                                label={'Top'}
-                                variant={'outlined'}
-                                type={"number"}
-                                className={classes.searchField}
-                                onChange={this.handleTopChange}
-                                value={this.props.top}
+                            <BBoxInput
+                                onBBoxChange={this.props.onBBoxChange}
+                                selectedBounds={this.props.selectedBounds}
                             />
                         </Grid>
                         <Grid item xs={12}>
-                            <WaveLengthSelect currentSelect={'All'}/>
+                            <WaveLengthSelect
+                                name={'Wavelength'}
+                                items={wavelengthItems}
+                                selectedItem={this.props.selectedWavelength}
+                                onChange={this.handleWaveLengthSelect}
+                            />
                         </Grid>
                     </Grid>
                     <DialogActions className={classes.appBar}>
