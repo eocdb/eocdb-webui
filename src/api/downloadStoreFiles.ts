4032cda829627615fe0d189c3d8cbc12845d7c76
Helge <helge.dzierzon@brockmann-consult.de>
2019-02-05 15:20:55 +0100

    - Added download APIs

--- src/api/downloadStoreFiles.ts
+++ src/api/downloadStoreFiles.ts
@@ -10,20 +10,9 @@ export function downloadStoreFiles(apiServerUrl: string, datasetQuery: DatasetQu
 }
 
 
-export function collectIdComponents(datasetQuery: DatasetQuery) {
-    let queryComponents = undefined;
-
-    if (datasetQuery.datasetIds) {
-        queryComponents = { id_list: datasetQuery.datasetIds };
-    }
-
-    return queryComponents;
-}
-
-export function downloadStoreFilesByIds(apiServerUrl: string, datasetQuery: DatasetQuery): Promise<Blob> {
-
-    const queryComponents = collectIdComponents(datasetQuery);
-    //const queryComponents = {id_list: ["5bf5451761d82d05dcd6cfb5", "5bf5451761d82d05dcd6cfb6"], docs: false};
+export function downloadStoreFilesByIds(apiServerUrl: string, selectedDatasets: string[], downloadDocs: boolean)
+    : Promise<Blob> {
+    const queryComponents = {id_list: selectedDatasets, docs: downloadDocs};
 
     return callBlobApi(apiServerUrl + '/store/download',
         undefined,
