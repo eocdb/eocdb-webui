0c97affba063251ea7c83b27b847b1f2cb64f67e
Helge <helge.dzierzon@brockmann-consult.de>
2019-02-07 17:58:03 +0100

    Renamed SubmitPanel to SubmissionPanel

---
+++ src/containers/submit/SubmissionPanel.tsx
@@ -0,0 +1,22 @@
+import { connect } from "react-redux";
+
+import { AppState } from "../../states/appState";
+import { closeSubmitSteps, openSubmitSteps } from "../../actions/submitActions";
+import SubmissionPanel from "../../components/submit/SubmissionPanel";
+
+
+const mapStateToProps = (state: AppState) => {
+    return {
+        submissionOpen: state.submitState.submissionOpen,
+    };
+};
+
+
+const mapDispatchToProps = {
+    closeSubmission: closeSubmitSteps,
+    openSubmission: openSubmitSteps,
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(SubmissionPanel)
+
