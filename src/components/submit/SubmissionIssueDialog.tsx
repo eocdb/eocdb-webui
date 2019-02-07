2b407f5803f45ee4feea7e4d72e2011aaffba4b7
Helge <helge.dzierzon@brockmann-consult.de>
2019-02-07 17:54:30 +0100

    Added a submission issue Dialog for reporting issues with data files to the user

---
+++ src/components/submit/SubmissionIssueDialog.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import { Theme, withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import Slide, { SlideProps } from '@material-ui/core/Slide';
+
+import createStyles from "@material-ui/core/styles/createStyles";
+import { WithStyles } from "@material-ui/core";
+import DialogTitle from "@material-ui/core/DialogTitle/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions/DialogActions";
+import { SubmissionForUserResult } from "../../api/getSubmissionFilesForUser";
+
+
+// noinspection JSUnusedLocalSymbols
+const styles = (theme: Theme) => createStyles({
+    appBar: {
+        position: 'relative',
+    },
+    flex: {
+        flex: 1,
+    },
+    searchField: {
+        width: 300,
+    },
+    textField: {},
+    button: {},
+    rightIcon: {},
+    tableContainer: {},
+});
+
+
+function Transition(props: SlideProps) {
+    return <Slide direction="up" {...props} />;
+}
+
+
+export interface SubmissionIssueDialogProps extends WithStyles<typeof styles> {
+    open: boolean;
+    onClose: () => void;
+
+    submissions: SubmissionForUserResult[];
+}
+
+
+class SubmissionIssueDialog extends React.Component<SubmissionIssueDialogProps> {
+    constructor(props: SubmissionIssueDialogProps) {
+        super(props);
+    }
+
+    render() {
+        const {classes} = this.props;
+        return (
+            <div>
+                <Dialog
+                    fullScreen
+                    open={this.props.open}
+                    onClose={this.props.onClose}
+                    TransitionComponent={Transition}
+                >
+                    <DialogTitle id="form-dialog-title">Submission Issue for</DialogTitle>
+                    <DialogActions className={classes.appBar}>
+                        <Button onClick={this.props.onClose}
+                                aria-label="Close"
+                                variant="contained"
+                                color="secondary"
+                        >
+                            Close
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+
+export default withStyles(styles)(SubmissionIssueDialog);
+
