3d30620f092313c7a3d85e2ab8068389c3417a35
Helge Dzierzon <42441761+dzelge@users.noreply.github.com>
2019-04-02 17:03:02 +0200

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 //export const DEFAULT_API_SERVER_URL = "http://10.2.0.57:4000/eocdb/api/v0.1.0";
 //export const DEFAULT_API_SERVER_URL = "http://localhost:4000/eocdb/api/v0.1.0";
-export const DEFAULT_API_SERVER_URL="https://www.brockmann-consult.de/eumetsat/ocdb-test/eocdb/api/v0.1.0";
-//export const DEFAULT_API_SERVER_URL = "http://eocdb-server:4000/eocdb/api/v0.1.0";
+//export const DEFAULT_API_SERVER_URL="https://www.brockmann-consult.de/eumetsat/ocdb-test/eocdb/api/v0.1.0";
+export const DEFAULT_API_SERVER_URL = "http://eocdb-server:4000/eocdb/api/v0.1.0";
 
 export const DEBUG = true;
