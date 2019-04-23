816b060a2a5ce7b0d3b11ee56cbd1cf9cdb57bc5
helge <helge.dzierzon@brockmann-consult.de>
2019-04-23 06:48:17 +0200

    added materila ui markdown

--- src/components/links/LinksPanel.tsx
+++ src/components/links/LinksPanel.tsx
@@ -3,11 +3,15 @@ import Typography from '@material-ui/core/Typography/Typography';
 import { Theme, WithStyles } from '@material-ui/core';
 import createStyles from '@material-ui/core/styles/createStyles';
 import { withStyles } from '@material-ui/core/styles';
+import ReactMarkdown from"markdown-to-jsx";
 
 // noinspection JSUnusedLocalSymbols
 const styles = (theme: Theme) => createStyles(
     {
         title: {},
+        listItem: {
+            marginTop: theme.spacing.unit,
+        },
     });
 
 interface LinksPanelProps extends WithStyles<typeof styles> {
@@ -19,6 +23,23 @@ class LinkaPanel extends React.PureComponent<LinksPanelProps> {
     constructor(props: LinksPanelProps) {
         super(props);
     }
+
+    options = () => {
+        overrides: {
+            h1: { component: props => <Typography gutterBottom variant="h4" {...this.props} /> },
+            h2: { component: props => <Typography gutterBottom variant="h6" {...this.props} /> },
+            h3: { component: props => <Typography gutterBottom variant="subtitle1" {...this.props} /> },
+            h4: { component: props => <Typography gutterBottom variant="caption" paragraph {...this.props} /> },
+            p: { component: props => <Typography paragraph {...this.props} /> },
+            li: {
+                component: withStyles(styles)(({ classes, ...props }) => (
+                    <li className={classes.listItem}>
+                        <Typography component="span" {...props} />
+                    </li>
+                )),
+            },
+        },
+    };
 
     render() {
         if (!this.props.show) {
@@ -37,7 +58,9 @@ class LinkaPanel extends React.PureComponent<LinksPanelProps> {
                 >
                     BROWSE
                 </Typography>
-                {this.props.content}
+                <ReactMarkdown options={options}>
+                    {this.props.content}
+                </ReactMarkdown>
             </div>
         );
     }
