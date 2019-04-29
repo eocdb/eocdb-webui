2b314db414e845ae5d14afd83f1a8c2b915bb8db
helge <helge.dzierzon@brockmann-consult.de>
2019-04-29 10:01:54 +0200

    - Login Dialog wasn't working anymore

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,8 +1,8 @@
 //export const DEFAULT_API_SERVER_URL = "http://10.2.0.57:4000/eocdb/api/v0.1.0";
-export const DEFAULT_API_SERVER_URL = "http://localhost:4000/eocdb/api/v0.1.0";
+//export const DEFAULT_API_SERVER_URL = "http://localhost:4000/eocdb/api/v0.1.0";
 //export const DEFAULT_API_SERVER_URL = "http://194.76.223.169/eocdb/api/v0.1.0";
 //export const DEFAULT_API_SERVER_URL="https://www.brockmann-consult.de/eumetsat/ocdb-test/eocdb/api/v0.1.0";
-//export const DEFAULT_API_SERVER_URL = "https://eocdb-server/eocdb/api/v0.1.0";
+export const DEFAULT_API_SERVER_URL = "https://eocdb-server/eocdb/api/v0.1.0";
 //export const DEFAULT_API_SERVER_URL = " http://194.76.223.169/eocdb/api/v0.1.0";
 
 export const DEBUG = true;
