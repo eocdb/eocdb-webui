a54c00af691c78c0a52b57f8a42f1537a63464a7
Helge Dzierzon <42441761+dzelge@users.noreply.github.com>
2019-04-30 16:04:35 +0200

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
 export const API_SERVER_VERSION='v0.1.0';
 
-export const DEFAULT_API_SERVER_URL = " http://ocdb.eumetsat.int/eocdb/api/" + API_SERVER_VERSION;
+export const SERVER_CONFIG = " http://ocdb.eumetsat.int/eocdb/api/" + API_SERVER_VERSION;
 
 export const DEBUG = true;
