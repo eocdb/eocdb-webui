d7f1e98687c79fb79ffb1bf200632473d3c43904
Helge <helge.dzierzon@brockmann-consult.de>
2019-05-27 14:44:28 +0200

    - Added credentials config in saveLinks to avoid CORS errors

--- src/api/saveLinks.ts
+++ src/api/saveLinks.ts
@@ -14,6 +14,7 @@ export function saveLinks(apiServerUrl: string, content: string): Promise<Links>
         undefined,
         {
             method: 'POST',
+            credentials: "same-origin",
             body: JSON.stringify(result),
         }
     );
