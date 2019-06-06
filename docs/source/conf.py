014a96a020c33c29901e665945121982e1877b71
Helge <helge.dzierzon@brockmann-consult.de>
2019-06-06 17:13:07 +0200

    - removed markdown parser to conf.py statement

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,13 +52,6 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 
-# for MarkdownParser
-from recommonmark.parser import MarkdownParser
-
-source_parsers = {
-    '.md': MarkdownParser,
-}
-
 source_suffix = ['.rst', '.md']
 # source_suffix = '.rst'
 
