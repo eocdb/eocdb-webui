e8549d0234889c9d83e3ef89f9652ccd83999ec4
Helge <helge.dzierzon@brockmann-consult.de>
2019-06-07 11:10:49 +0200

    Added sphinx-markdown-tables to extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,8 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'recommonmark'
+    'recommonmark',
+    'sphinx-markdown-tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
