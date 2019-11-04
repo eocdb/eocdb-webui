e64d5872784d8dd4d4c20143d71b49720127923c
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-11-04 09:59:46 +0100

    Added a staging deployment

--- src/api/config.ts
+++ src/api/config.ts
@@ -6,6 +6,9 @@ let baseUrl = 'http://localhost/ocdb-server';
 if (process.env.NODE_ENV === 'production') {
     baseUrl = "https://ocdb.eumetsat.int";
 }
+else if(process.env.NODE_ENV == 'staging'){
+    baseUrl = "https://ocdb.eumetsat.int/stage";
+}
 
 export const SERVER_CONFIG = baseUrl + "/ocdb/api/" + API_SERVER_VERSION_TAG;
 
