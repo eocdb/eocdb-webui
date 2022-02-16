ffb2b78ccdb1a8ebf4fa590f11b1c1a5f9c9612a
dzelge <helge.dzierzon@brockmann-consult.de>
2022-02-16 11:14:42 +0100

    Fixed submission styles

--- src/components/submission/SubmissionDialog.tsx
+++ src/components/submission/SubmissionDialog.tsx
@@ -1,4 +1,13 @@
-import { Checkbox, FormControlLabel, TextField, Icon, IconButton, Button, Grid, FormControl } from "@mui/material";
+import {
+    Checkbox,
+    FormControlLabel,
+    TextField,
+    Icon,
+    IconButton,
+    Button,
+    Grid,
+    Stack, styled, Paper
+} from "@mui/material";
 import * as React from "react";
 import FileUpload from "./FileUpload";
 import { CloudUpload } from "@mui/icons-material";
@@ -44,6 +53,15 @@ interface SubmissionDialogProps {
     submissionMessages: MessageLogEntry[];
     onHideSubmissionMessages: (id: number) => void;
 }
+
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 
 class SubmissionDialog extends React.Component<SubmissionDialogProps> {
@@ -131,66 +149,70 @@ class SubmissionDialog extends React.Component<SubmissionDialogProps> {
                     </HelpDialog>
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField
-                        required
-                        id="outlined-dense"
-                        label="Submission Label"
-                        margin="dense"
-                        variant="outlined"
-                        onChange={this.handleSubmissionIdChange}
-                        value={this.props.submissionIdValue}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        required
-                        id="submission-path"
-                        label="Submission Path"
-                        margin="dense"
-                        variant="outlined"
-                        onChange={this.handleOnPathChange}
-                        value={this.props.pathValue}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <DatePicker
-                        value={this.props.publicationDate}
-                        onChange={this.props.onPublicationDateChange}
-                        renderInput={(params) => <TextField {...params} helperText={null} />}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <FormControlLabel
-                        control={<Checkbox
-                            value={'publish'}
-                            checked={this.props.allowPublication}
-                            onChange={this.handlePublicationDateChange}
+                    <Stack direction={'column'}>
+                        <Item>
+                        <TextField
+                            required
+                            id="outlined-dense"
+                            label="Submission Label"
+                            margin="dense"
+                            variant="outlined"
+                            onChange={this.handleSubmissionIdChange}
+                            value={this.props.submissionIdValue}
                         />
-                        }
-                        label={'(By agreeing to the publication of the submitted in-situ data, you accept the general public being able to access these data. If you do not agree to the publication of your data then the data will be only visible to you and the OCDB administrators. Please refer to the Data Policy section of the User Manual for more information)'}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <FileUpload
-                        key={'drop-datafiles'}
-                        label={'Drag and drop DATA files here or click'}
-                        onChange={this.handleOndropDatafiles}
-                        files={this.props.dataFilesValue}
-                        onDropRejected={this.props.onDropRejected}
-                        acceptedFiles={['text/plain', 'text/csv', 'text/x-csv','application/vnd.ms-excel',
-                            'application/csv', 'application/x-csv', 'text/comma-separated-values',
-                            'text/x-comma-separated-values', 'text/tab-separated-values']}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <FileUpload
-                        key={'drop-docfiles'}
-                        onDropRejected={this.props.onDropRejected}
-                        label={'Drag and drop DOCUMENT files here or click'}
-                        onChange={this.handleOndropDocfiles}
-                        files={this.props.docFilesValue}
-                        acceptedFiles={undefined}
-                    />
+                        </Item>
+                        <Item>
+                        <TextField
+                            required
+                            id="submission-path"
+                            label="Submission Path"
+                            margin="dense"
+                            variant="outlined"
+                            onChange={this.handleOnPathChange}
+                            value={this.props.pathValue}
+                        />
+                        </Item>
+                        <Item>
+                        <DatePicker
+                            value={this.props.publicationDate}
+                            onChange={this.props.onPublicationDateChange}
+                            renderInput={(params) => <TextField {...params} helperText={null} />}
+                        />
+                        </Item>
+                        <Item>
+                        <FormControlLabel
+                            control={<Checkbox
+                                value={'publish'}
+                                checked={this.props.allowPublication}
+                                onChange={this.handlePublicationDateChange}
+                            />
+                            }
+                            label={'(By agreeing to the publication of the submitted in-situ data, you accept the general public being able to access these data. If you do not agree to the publication of your data then the data will be only visible to you and the OCDB administrators. Please refer to the Data Policy section of the User Manual for more information)'}
+                        />
+                        </Item>
+                        <Item>
+                        <FileUpload
+                            key={'drop-datafiles'}
+                            label={'Drag and drop DATA files here or click'}
+                            onChange={this.handleOndropDatafiles}
+                            files={this.props.dataFilesValue}
+                            onDropRejected={this.props.onDropRejected}
+                            acceptedFiles={['text/plain', 'text/csv', 'text/x-csv','application/vnd.ms-excel',
+                                'application/csv', 'application/x-csv', 'text/comma-separated-values',
+                                'text/x-comma-separated-values', 'text/tab-separated-values']}
+                        />
+                        </Item>
+                        <Item>
+                        <FileUpload
+                            key={'drop-docfiles'}
+                            onDropRejected={this.props.onDropRejected}
+                            label={'Drag and drop DOCUMENT files here or click'}
+                            onChange={this.handleOndropDocfiles}
+                            files={this.props.docFilesValue}
+                            acceptedFiles={undefined}
+                        />
+                        </Item>
+                    </Stack>
                 </Grid>
                 <MessageLog
                     messages={this.props.submissionMessages}
