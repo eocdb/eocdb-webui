ef52756cd97ead6195ea9f6c67568de443236b5b
dzelge <helge.dzierzon@brockmann-consult.de>
2022-02-24 10:23:21 +0100

    Adding filter model

--- src/api/getSubmissionsForUser.ts
+++ src/api/getSubmissionsForUser.ts
@@ -1,27 +1,50 @@
 import { callJsonApi, QueryComponent } from './callApi';
 import { SubmissionQuery, SubmissionResult } from "../model/Submission";
 
-export function collectComponents(offset: number, count: number) {
+export function collectComponents(query?: SubmissionQuery) {
     const queryComponents: QueryComponent[] = [];
-    if (offset) {
-        queryComponents.push(['offset', JSON.stringify(offset)]);
+    if (!query) {
+        return queryComponents;
     }
-    if (count) {
-        queryComponents.push(['count', JSON.stringify(count)]);
+    if (query.user_id) {
+        queryComponents.push(['user-id', JSON.stringify(query.user_id)]);
     }
+    if (query.offset) {
+        queryComponents.push(['offset', JSON.stringify(query.offset)]);
+    }
+    if (query.count) {
+        queryComponents.push(['count', JSON.stringify(query.count)]);
+    }
+
+    if (query.filterModel) {
+        const {filterModel} = query;
+        const value = filterModel.items[0].value;
+        let column = filterModel.items[0].columnField;
+        column = column == 'id'? 'submission_id' : column;
+
+        queryComponents.push(['query-column', JSON.stringify(column)]);
+        queryComponents.push(['query-value', JSON.stringify(value)]);
+    }
+
+    if (query.sortModel) {
+        const {sortModel} = query;
+        const sortColumn = sortModel[0].field;
+        const sortOrder = sortModel[0].sort;
+
+        queryComponents.push(['sort-column', JSON.stringify(sortColumn)]);
+        queryComponents.push(['sort-order', JSON.stringify(sortOrder)]);
+    }
+
     return queryComponents;
 }
-
 
 /**
  *
  * @param apiServerUrl: URL of the OCDB API
- * @param user: ID of the user
- * @param offset: offset for pagination
- * @param count: count for pagination
+ * @param submissionQuery: query/filter for submissions
  */
 export function getSubmissionsForUser(apiServerUrl: string, submissionQuery: SubmissionQuery): Promise<SubmissionResult> {
-    const queryComponents = collectComponents(offset, count)
+    const queryComponents = collectComponents(submissionQuery);
     return callJsonApi<SubmissionResult>(
         apiServerUrl + '/store/upload/user', queryComponents,
         {
