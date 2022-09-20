8fb8629da10cddfdc402a03d75f70dd947a26d82
sabine <sabine.embacher@brockmann-consult.de>
2022-09-20 05:40:12 +0200

    Parameter documentation added

--- src/api/getSubmissionFile.ts
+++ src/api/getSubmissionFile.ts
@@ -6,6 +6,7 @@ import { SubmissionFile } from "../model";
  *
  * @param apiServerUrl: URL of the OCDB API
  * @param submissionId: ID of the submission
+ * @param submissionFileIndex: Index of the submission file
  */
 export function getSubmissionFile(apiServerUrl: string,
                                   submissionId: string,
