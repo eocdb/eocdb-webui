1d4d0a5c0b17b0e2263c7cfcdb48072961667b48
sabine <sabine.embacher@brockmann-consult.de>
2023-10-17 07:56:38 +0200

    set brockmann frm4soc backup url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,7 @@
 export const API_SERVER_VERSION='0.1.9';
 export const API_SERVER_VERSION_TAG='latest';
 
+const DEFAULT_API_ENDPOINT_BACKUP_BC = 'https://www.brockmann-consult.de/frm4soc-bkp';
 const DEFAULT_API_ENDPOINT_PRODUCTION = 'https://ocdb.eumetsat.int';
 const DEFAULT_API_ENDPOINT_STAGE = 'https://ocdb-stage.eumetsat.int';
 // NOT used at this stage
@@ -11,19 +12,20 @@ const DEF_API_ENDPOINT_OCDB_BACK = 'http://frm4soc';
 function getEndpointUrl(): string {
     let url = process.env.REACT_APP_CATEHUB_ENDPOINT;
     if (!url) {
-        const host = window.location.host;
-        if (host.indexOf('stage') >= 0) {
-            url = DEFAULT_API_ENDPOINT_STAGE;
-        } else if (host.indexOf('dev') >= 0) {
-            url = DEFAULT_API_ENDPOINT_DEV;
-        } else if (host.indexOf('localhost') >= 0) {
-            url = DEFAULT_API_ENDPOINT_LOCAL;
+        // const host = window.location.host;
+        // if (host.indexOf('stage') >= 0) {
+        //     url = DEFAULT_API_ENDPOINT_STAGE;
+        // } else if (host.indexOf('dev') >= 0) {
+        //     url = DEFAULT_API_ENDPOINT_DEV;
+        // } else if (host.indexOf('localhost') >= 0) {
+        //     url = DEFAULT_API_ENDPOINT_LOCAL;
         // } else if (host.indexOf('frm4soc') >= 0) {
-        //     url = window.location.origin;
-        } else {
+            url = DEFAULT_API_ENDPOINT_BACKUP_BC;
+            // url = window.location.href;
+        // } else {
             // url = DEFAULT_API_ENDPOINT_PRODUCTION;
-                url = window.location.origin;
-        }
+            //     url = window.location.origin;
+        // }
     }
     return url.endsWith('/') ? url.substr(0, url.length - 1) : url;
 }
